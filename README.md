6e33a210ebf158ef28abb945401c875d4db4a0f5
kaiodeodato <kaiod.oficial@gmail.com>
2024-06-17 23:02:22 +0100

    template literals

--- README.md
+++ README.md
@@ -1,3 +1,65 @@
-# kaioextension README
+### Snippeting: Your Code Efficiency Companion**
 
-**Enjoy!**
+**Tired of repetitive typing and searching for snippets?** 
+
+Introducing Snippeting, your one-stop shop for effortless code completion and enhanced productivity.  With Snippeting, you can create a personalized collection of snippets and variables, readily accessible at your fingertips, empowering you to code with speed and efficiency. ⚡️
+
+**Effortless Snippet Management**  
+
+1. **Create Your Configuration File:**
+
+   In your project's root directory, create a file named `sconfig.js`. This file will serve as your snippet repository.
+
+2. **Define Your Snippets:**   
+
+   Use the following format to define your snippets in `sconfig.js`:
+
+   ```javascript
+   module.exports = [
+     { "key1": "snippet1" },
+     { "key2": "snippet2" },
+     // ... more snippets
+   ];
+
+Key: Choose a short and descriptive name for your snippet (e.g., myReactForm).
+Snippet: Enter the actual code you want to insert (e.g., complete React form component).
+Advanced Snippet Examples:    ️
+
+Showcase the versatility of Snippeting with more complex snippets:
+
+ ```javascript
+module.exports = [
+  {
+    "myReactForm": `import React, { useState } from 'react';
+
+const Form = () => {
+  const [value, setValue] = useState('');
+
+  const handleChange = (e) => setValue(e.target.value);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={value} onChange={handleChange} />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default Form;`
+  },
+
+  // ... other snippets
+];
+```
+
+As you start typing a snippet key in your code, Snippeting will intelligently suggest the entire snippet for quick insertion. This saves you time and effort, allowing you to focus on the core logic of your code.
+
+
+Install Snippeting today and experience a significant boost in your coding efficiency. Enjoy the convenience of having your essential snippets readily available, empowering you to code with speed and precision.  ️
+
+🎉 Happy Coding!
--- extension.js
+++ extension.js
@@ -6,108 +6,101 @@ let kconfigContent = [];
 let disposableProvider = null;
 
 function activate(context) {
-    const fileName = 'sconfig.js';
-    const workspaceFolders = vscode.workspace.workspaceFolders;
+  const fileName = 'sconfig.js';
+  const workspaceFolders = vscode.workspace.workspaceFolders;
 
-    if (workspaceFolders && workspaceFolders.length > 0) {
-        const firstWorkspaceFolder = workspaceFolders[0];
-        const filePath = path.join(firstWorkspaceFolder.uri.fsPath, fileName);
-        const watcher = vscode.workspace.createFileSystemWatcher(filePath);
+  if (workspaceFolders && workspaceFolders.length > 0) {
+    const firstWorkspaceFolder = workspaceFolders[0];
+    const filePath = path.join(firstWorkspaceFolder.uri.fsPath, fileName);
+    const watcher = vscode.workspace.createFileSystemWatcher(filePath);
 
-        const updateKconfigContent = (filePath) => {
-            fs.readFile(filePath, 'utf8', (err, data) => {
-                if (err) {
-                    vscode.window.showErrorMessage(`Failed to read ${filePath}: ${err.message}`);
-                    return;
-                }
-                const newContent = JSON.parse(data);
-                if (!arraysAreEqual(kconfigContent, newContent)) {
-                    kconfigContent = newContent;
-                    registerCompletionProviders();
-                    console.log(`Conteúdo do arquivo ${filePath}:`, kconfigContent);
-                }
-            });
-        };
-
-        updateKconfigContent(filePath);
-
-        watcher.onDidChange((uri) => {
-            console.log("Escrevi em kconfig.js");
-            vscode.window.showInformationMessage("Escrevi em kconfig.js");
-            updateKconfigContent(uri.fsPath);
-            console.log(uri.fsPath);
+    const updateKconfigContent = (filePath) => {
+        console.log("refresh")
+        delete require.cache[require.resolve(filePath)];
+        fs.readFile(filePath, 'utf8', (err, data) => {
+            console.log("data")
+            console.log(data)
+          if (err) {
+            vscode.window.showErrorMessage(`Failed to read ${filePath}: ${err.message}`);
+            return; // Exit the function if there's an error
+          }
+          try {
+            kconfigContent = require(filePath);
+            console.log("kconfigContent")
+            console.log(kconfigContent)
+          } catch (error) {
+            vscode.window.showErrorMessage(`Error parsing ${filePath}: ${error.message}`);
+            kconfigContent = []; // Set kconfigContent to empty array on parsing error
+          }
+          registerCompletionProviders();
         });
+      };
 
-        context.subscriptions.push(watcher);
+    updateKconfigContent(filePath);
+    watcher.onDidChange(() => {
+        console.log(kconfigContent)
+        updateKconfigContent(filePath);
+      });
 
-    } else {
-        vscode.window.showErrorMessage('No workspace folder found.');
-    }
+    context.subscriptions.push(watcher);
+  } else {
+    vscode.window.showErrorMessage('No workspace folder found.');
+  }
 }
 
 function registerCompletionProviders() {
-    disposeCompletionProviders();
+  disposeCompletionProviders();
 
-    disposableProvider = vscode.languages.registerCompletionItemProvider(
-        { language: '*', scheme: 'file' },
-        {
-            provideCompletionItems: provideCompletionItems
-        }
-    );
+  disposableProvider = vscode.languages.registerCompletionItemProvider(
+    { language: '*', scheme: 'file' },
+    {
+      provideCompletionItems: provideCompletionItems
+    }
+  );
 
-    context.subscriptions.push(disposableProvider);
+  context.subscriptions.push(disposableProvider);
 }
 
 function provideCompletionItems(document, position) {
-    const range = document.getWordRangeAtPosition(position);
-    const inputText = document.getText(range);
+  const range = document.getWordRangeAtPosition(position);
+  const inputText = document.getText(range);
 
-    const suggestions = [];
-    for (let i = 0; i < kconfigContent.length; i++) {
-        const key = Object.keys(kconfigContent[i])[0];
-        const value = kconfigContent[i][key];
-        
-        if (inputText === key) {
-            suggestions.push(
-                createCompletionItem(
-                    `${key}\n${value}`,
-                    'component react'
-                )
-            );
-        }
+  const suggestions = [];
+  for (let i = 0; i < kconfigContent.length; i++) {
+    const key = Object.keys(kconfigContent[i])[0];
+    const value = kconfigContent[i][key];
+
+    if (inputText === key) {
+      suggestions.push(
+        createCompletionItem(
+          `${key}\n${value}`,
+          'component react'
+        )
+      );
     }
-
-    return suggestions;
+  }
+  return suggestions;
 }
 
 function createCompletionItem(label, detail) {
-    const item = new vscode.CompletionItem(label, vscode.CompletionItemKind.Value);
-    item.detail = detail;
-    item.insertText = new vscode.SnippetString(label);
-    return item;
+  const item = new vscode.CompletionItem(label, vscode.CompletionItemKind.Value);
+  item.detail = detail;
+  item.insertText = new vscode.SnippetString(label);
+  return item;
 }
 
 function disposeCompletionProviders() {
-    if (disposableProvider) {
-        disposableProvider.dispose();
-        disposableProvider = null;
-    }
+  if (disposableProvider) {
+    disposableProvider.dispose();
+    disposableProvider = null;
+  }
 }
 
 function deactivate() {
-    disposeCompletionProviders();
+  disposeCompletionProviders();
 }
 
 module.exports = {
-    activate,
-    deactivate
+  activate,
+  deactivate
 };
-
-function arraysAreEqual(arr1, arr2) {
-    if (arr1.length !== arr2.length) return false;
-    for (let i = 0; i < arr1.length; i++) {
-        if (JSON.stringify(arr1[i]) !== JSON.stringify(arr2[i])) return false;
-    }
-    return true;
-}
-
